a6c7f2f4d2b4febfc5b9569e3239ddda21f783eb
BoppreH <boppreh@gmail.com>
2015-05-30 21:59:02 -0300

    Allow math names in begining and end of lines

--- generic.py
+++ generic.py
@@ -126,7 +126,7 @@ def include_math(match):
     text = match.group(1).strip()
     for rule, replacement in math_rules:
         text = text.replace(rule, replacement)
-    text = re.sub(r'([^a-zA-Z])(log|sin|cos|tan|lim|gcd|ln)([^a-zA-Z])', r'\1\\\2\3', text)
+    text = re.sub(r'(^|[^a-zA-Z])(log|sin|cos|tan|lim|gcd|ln)([^a-zA-Z]|$)', r'\1\\\2\3', text)
     if '\n' in text:
         text = re.sub(r'\n+', r'\\\\', text).strip('\n')
         return '\\begin{gather*}' + text + '\\end{gather*}'
